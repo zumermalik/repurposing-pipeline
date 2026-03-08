96dad3a602835ea6ae2cef9473d9c2f7c818a90b
zhq0 <zumermalik48@gmail.com>
2026-03-08 11:32:36 +0500

    Create logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+def setup_logger(name: str) -> logging.Logger:
+    """
+    Configures and returns a standard logger for the pipeline.
+    """
+    logger = logging.getLogger(name)
+    
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
+    return logger
